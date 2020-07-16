b2b1d3b00a70f5e438fee272905a232c1f25dbb5
AnandCodeX <anandrtiwari@live.com>
2020-07-16 14:44:56 +0530

    complete-intro V2

--- src/Component/Typo/Typo1.js
+++ src/Component/Typo/Typo1.js
@@ -9,85 +9,56 @@ const Typo1 = (props) => {
     <Auxi>
       <div>
         <span className="typo1">
-          {" "}
-          <Abt style={{ fontSize: "inherit", color: "white" }}>H</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>i</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>H</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>i</Abt>
           <Abt style={{ paddingLeft: "10px" }}>{"  "}</Abt>
         </span>
         <span className="typo1">
-          {" "}
-          <Abt style={{ fontSize: "inherit", color: "white" }}>,</Abt>
-        </span>
-      </div>
-      <div>
-        {" "}
-        <span className="typo1">
-          {" "}
-          <Abt style={{ fontSize: "inherit", color: "white" }}>I</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>'</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>m</Abt>
-        </span>
-        <span className="Name">
-          <Abt style={{ paddingLeft: "10px" }}>{"  "}</Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            A
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            n
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            a
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            n
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            d
-          </Abt>
-          <Abt style={{ paddingLeft: "10px" }}>{"  "}</Abt>
-        </span>
-        <span className="Name">
-          {" "}
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            T
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            i
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            w
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            a
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            r
-          </Abt>
-          <Abt style={{ fontSize: "inherit", color: "rgb(255, 136, 0)" }}>
-            i
-          </Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>,</Abt>
         </span>
       </div>
       <div>
         <span className="typo1">
-          {" "}
-          <Abt style={{ fontSize: "inherit", color: "white" }}>M</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>E</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>R</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>N</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>I</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>'</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>m</Abt>
+        </span>
+        <span className="Name">
+          <Abt style={{ paddingLeft: "20px" }}>{"  "}</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>A</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>n</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>a</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>n</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>d</Abt>
+          <Abt style={{ paddingLeft: "10px" }}>{"  "}</Abt>
+        </span>
+        <span className="Name">
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>T</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>i</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>w</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>a</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>r</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>i</Abt>
+        </span>
+      </div>
+      <div>
+        <span className="typo1">
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>M</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>E</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>R</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>N</Abt>
         </span>
         <span className="typo1">
-          {" "}
           <Abt style={{ paddingLeft: "10px" }}>{"  "}</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>D</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>e</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>v</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>e</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>l</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>o</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>p</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>e</Abt>
-          <Abt style={{ fontSize: "inherit", color: "white" }}>r</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>D</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>e</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>v</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>e</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>l</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>o</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>p</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>e</Abt>
+          <Abt style={{ fontSize: "inherit", color: "inherit" }}>r</Abt>
         </span>
         <p>MongoDB / ExpressJS / ReactJS / NodeJS</p>
       </div>
